2a9b8761acc67245f46dffaf19b565eddd2adea9
Caroline <carolinesitter24@gmail.com>
2022-04-15 17:43:30 -0500

    Wrote test_no_rsvp_yet Test

--- tests.py
+++ tests.py
@@ -16,15 +16,20 @@ class PartyTests(unittest.TestCase):
         self.assertIn(b"board games, rainbows, and ice cream sundaes", result.data)
 
     def test_no_rsvp_yet(self):
-        # FIXME: Add a test to show we see the RSVP form, but NOT the
+        # Add a test to show we see the RSVP form, but NOT the
         # party details
-        print("FIXME")
+
+        result = self.client.get("/")
+        self.assertIn(b"Please RSVP", result.data)
+        self.assertNotIn(b"Party Details", result.data)
+
+        print(result)
 
     def test_rsvp(self):
         result = self.client.post("/rsvp",
-                                  data={"name": "Jane",
+                                    data={"name": "Jane",
                                         "email": "jane@jane.com"},
-                                  follow_redirects=True)
+                                    follow_redirects=True)
         # FIXME: Once we RSVP, we should see the party details, but
         # not the RSVP form
         print("FIXME")
